28b4b8404196bcbee253bc0f7b73703266ed189e
jaime <garciadiazjaime@gmail.com>
2022-01-25 19:41:38 -0600

    feat: adjust title and image

--- modules/excelsior/article-etl.js
+++ modules/excelsior/article-etl.js
@@ -6,13 +6,13 @@ const { getHTMLLean } = require('../support/extract');
 function transform(html, item) {
   const $ = cheerio.load(html);
 
-  const title = $('h1.node-title').text();
+  const title = $('main h1').text();
   const description = $('article #node-article-body p, article #id-body-node p')
     .toArray()
     .map((desc) => $(desc).text().replace('\n', '').trim())
     .filter((desc) => desc.length > 7 && !desc.includes('La ley de derechos') && !desc.includes('En el siguiente enlace') && !desc.includes('Puedes leer la nota'));
 
-  const image = $('figure.main-image img').attr('src');
+  const image = $('figure.main-image img').length ? $('figure.main-image img').attr('src') : $('#content-main article img.single-image').attr('src');
 
   const article = {
     ...item,
